654ce0bbab5a3d2515b86b4395e8a48d406ef68c
Jake Samuels <samuels242@googlemail.com>
2023-04-25 12:40:47 +0100

    Import and use LoadingProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { IsLoadingProvider } from "./context/IsLoadingContext.jsx";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <IsLoadingProvider>
+        <App />
+      </IsLoadingProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
