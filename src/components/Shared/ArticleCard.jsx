88f5901d07fc78d0dd27f478b46cdc100202d167
Jake Samuels <samuels242@googlemail.com>
2023-04-25 12:19:24 +0100

    Add link to Single Article Page

--- src/components/Shared/ArticleCard.jsx
+++ src/components/Shared/ArticleCard.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 const ArticleCard = ({ article }) => {
   return (
     <div className="article-card">
       <img src={article.article_img_url} alt="" className="article-card__img" />
 
-      <p className="article-card__title">{article.title}</p>
+      <Link to={`/articles/${article.article_id}`}>
+        <p className="article-card__title">{article.title}</p>
+      </Link>
 
       <p className="topic-link">{article.topic}</p>
     </div>
