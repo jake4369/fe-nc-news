352790d4857a88540adfcfa660669e55d249db00
Jake Samuels <samuels242@googlemail.com>
2023-04-25 13:58:00 +0100

    Get TopThreeArticle data from API

--- src/components/Shared/TopThreeArticles.jsx
+++ src/components/Shared/TopThreeArticles.jsx
@@ -1,23 +1,44 @@
-import { useState } from "react";
-import TopThreeArticlesData from "../../articlesData/topThreeArticles";
+import { useEffect, useState } from "react";
+import { getAllArticles } from "../../utils/api";
+import { useIsLoading } from "../../context/IsLoadingContext";
+
+import LoadingSpinner from "./LoadingSpinner";
 
 const TopThreeArticles = () => {
-  const [topThreeArticles, setTopThreeArticles] =
-    useState(TopThreeArticlesData);
+  const [topThreeArticles, setTopThreeArticles] = useState([]);
+  const { isLoading, setIsLoading } = useIsLoading();
 
-  const topArticles = topThreeArticles.map((obj) => {
+  useEffect(() => {
+    setIsLoading(true);
+    getAllArticles().then((articlesData) => {
+      const sortedArticles = articlesData.sort((a, b) => b.votes - a.votes);
+
+      setTopThreeArticles(sortedArticles.slice(0, 3));
+
+      setIsLoading(false);
+    });
+  }, []);
+
+  const topArticles = topThreeArticles.map((article, index) => {
     return (
-      <div className="top-article" key={obj.id}>
-        <img src={obj.image} alt="" className="top-article__img" />
+      <div className="top-article" key={article.article_id}>
+        <img
+          src={article.article_img_url}
+          alt=""
+          className="top-article__img"
+        />
         <div className="top-article__text-container">
-          <span className="top-article__number">0{obj.id}</span>
-          <h3 className="top-article__heading">{obj.heading}</h3>
-          <p className="top-article__text">{obj.text}</p>
+          <span className="top-article__number">0{index + 1}</span>
+          <h3 className="top-article__heading">{article.title}</h3>
         </div>
       </div>
     );
   });
-  return <div className="top-articles__container">{topArticles}</div>;
+  return (
+    <div className="top-articles__container">
+      {isLoading ? <LoadingSpinner /> : topArticles}
+    </div>
+  );
 };
 
 export default TopThreeArticles;
