c97cc41fb428503347321339175c5fb4a43ea526
Jake Samuels <samuels242@googlemail.com>
2023-04-25 12:18:38 +0100

    Add route to Single Article Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Homepage from "./pages/Homepage";
 import AllArticlesPage from "./pages/AllArticlesPage";
+import SingleArticlePage from "./pages/SingleArticlePage";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route element={<Layout />}>
           <Route path="/" element={<Homepage />} />
           <Route path="/articles" element={<AllArticlesPage />} />
+          <Route path="/articles/:article_id" element={<SingleArticlePage />} />
         </Route>
       </Routes>
     </main>
