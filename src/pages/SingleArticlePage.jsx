5910ecef9d43e65535b02fd1e9ec3f38b8486767
Jake Samuels <samuels242@googlemail.com>
2023-04-25 13:50:00 +0100

    Render Comments section

--- src/pages/SingleArticlePage.jsx
+++ src/pages/SingleArticlePage.jsx
@@ -3,6 +3,7 @@ import { useIsLoading } from "../context/IsLoadingContext";
 import SingleArticle from "../components/SingleArticlePage/SingleArticle";
 import NewArticles from "../components/Shared/NewArticles";
 import TopThreeArticles from "../components/Shared/TopThreeArticles";
+import CommentSection from "../components/SingleArticlePage/CommentSection/CommentSection";
 
 const SingleArticlePage = () => {
   const { isLoading, setIsLoading } = useIsLoading();
@@ -15,6 +16,8 @@ const SingleArticlePage = () => {
       >
         <section className="articles-column">
           <SingleArticle />
+
+          <CommentSection />
         </section>
 
         <NewArticles />
