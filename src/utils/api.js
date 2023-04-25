b1c948303fb2a7fc572f8a0ed6879a9c34887a5d
Jake Samuels <samuels242@googlemail.com>
2023-04-25 12:18:09 +0100

    getArticle util function

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,11 +4,21 @@ const newsAPI = axios.create({
   baseURL: "https://nc-news-api-aqzx.onrender.com/api",
 });
 
+// ARTICLES
 export const getAllArticles = () => {
   return newsAPI
     .get("/articles")
     .then(({ data }) => {
       return data.articles;
+    })
+    .catch((error) => console.error(error));
+};
+
+export const getArticle = (article_id) => {
+  return newsAPI
+    .get(`/articles/${article_id}`)
+    .then(({ data }) => {
+      return data.article;
     })
     .catch((error) => console.error(error));
 };
