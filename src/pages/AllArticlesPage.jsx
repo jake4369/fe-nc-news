4942db2551c6cb15f90b4dd862263877b9382b5f
Jake Samuels <samuels242@googlemail.com>
2023-04-25 10:51:37 +0100

    Render most popular article

---
+++ src/pages/AllArticlesPage.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { getAllArticles } from "../utils/api";
+
+import MostPopularArticle from "../components/AllArticlesPage/MostPopularArticle";
+import NewArticles from "../components/Shared/NewArticles";
+import TopThreeArticles from "../components/Shared/TopThreeArticles";
+
+const AllArticlesPage = () => {
+  const [allArticles, setAllArticles] = useState([]);
+  const [mostPopularArticle, setMostPopularArticle] = useState(null);
+
+  useEffect(() => {
+    getAllArticles().then((articlesData) => {
+      const sortedArticles = articlesData.sort((a, b) => b.votes - a.votes);
+
+      setMostPopularArticle(sortedArticles[0]);
+      setAllArticles(sortedArticles.slice(1));
+    });
+  }, []);
+
+  return (
+    <div className="all-articles-page">
+      <div className="flex-container">
+        <section className="articles-column">
+          <MostPopularArticle article={mostPopularArticle} />
+        </section>
+
+        <NewArticles />
+      </div>
+    </div>
+  );
+};
+
+export default AllArticlesPage;
