5c934cc5a520f4fec1f4c110ee57b2b2c14777d7
Jake Samuels <samuels242@googlemail.com>
2023-04-25 11:29:43 +0100

    All articles section

--- src/components/AllArticlesPage/AllArticles.jsx
+++ src/components/AllArticlesPage/AllArticles.jsx
@@ -1,5 +1,11 @@
-const AllArticles = () => {
-  return <div></div>;
+import ArticleCard from "../Shared/ArticleCard";
+
+const AllArticles = ({ articles }) => {
+  const articleCards = articles.map((article) => {
+    return <ArticleCard key={article.article_id} article={article} />;
+  });
+
+  return <section className="all-article-section">{articleCards}</section>;
 };
 
 export default AllArticles;
