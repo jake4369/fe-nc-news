948c46e06e4c9f4853762bcf32875525fe26e148
Jake Samuels <samuels242@googlemail.com>
2023-04-28 09:40:40 +0100

    Post comment form

---
+++ src/components/SingleArticlePage/CommentSection/CommentForm.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { addComment } from "../../../utils/api";
+
+import { useUser } from "../../../context/UserContext";
+
+const CommentForm = ({ setNewCommentPosted, commentsContainerRef }) => {
+  const { loggedInUser } = useUser();
+  const { article_id } = useParams();
+  const [commentBody, setCommentBody] = useState("");
+
+  const handleChange = (e) => {
+    setCommentBody(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    setNewCommentPosted(false);
+    e.preventDefault();
+
+    if (loggedInUser !== null) {
+      if (commentBody !== "") {
+        addComment(article_id, loggedInUser.username, commentBody).then(() => {
+          setNewCommentPosted(true);
+          setCommentBody("");
+          commentsContainerRef.current.scrollIntoView();
+        });
+      } else {
+        alert("Comment must not be empty");
+
+        return;
+      }
+    } else {
+      alert("Please log in to add a comment");
+      setCommentBody("");
+    }
+  };
+
+  return (
+    <form className="comment-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="comment-form__input"
+        value={commentBody}
+        onChange={handleChange}
+        placeholder="Leave a comment..."
+      />
+
+      <button className="comment-form__submit-btn">Send</button>
+    </form>
+  );
+};
+
+export default CommentForm;
