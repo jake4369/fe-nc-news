ef8227beea93fd30c00ac9a5bc09dc5d6402e663
Jake Samuels <samuels242@googlemail.com>
2023-04-28 09:40:17 +0100

    Import and use UserProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { IsLoadingProvider } from "./context/IsLoadingContext.jsx";
+import { UserProvider } from "./context/UserContext.jsx";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <IsLoadingProvider>
-        <App />
-      </IsLoadingProvider>
+      <UserProvider>
+        <IsLoadingProvider>
+          <App />
+        </IsLoadingProvider>
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
