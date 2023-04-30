4b47cd588324790fec688d498ce58432a0e1c87a
Jake Samuels <samuels242@googlemail.com>
2023-04-30 14:02:11 +0100

    Get new articles from API

--- src/components/Shared/NewArticles.jsx
+++ src/components/Shared/NewArticles.jsx
@@ -1,14 +1,37 @@
-import { useState } from "react";
-import articleData from "../../articlesData/newArticles";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
+import { getAllArticles } from "../../utils/api";
 
 const NewArticles = () => {
-  const [newArticles, setNewArticles] = useState(articleData);
+  const [newArticles, setNewArticles] = useState([]);
 
-  const articles = newArticles.map((obj) => {
+  useEffect(() => {
+    getAllArticles(null, "created_at", "desc").then((articlesData) => {
+      setNewArticles(articlesData.slice(0, 3));
+    });
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
+  const articles = newArticles.map((article) => {
     return (
-      <div className="new-article" key={obj.id}>
-        <h3 className="new-article__heading">{obj.heading}</h3>
-        <p className="new-article__text">{obj.text}</p>
+      <div className="new-article" key={article.article_id}>
+        <AnimatePresence>
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+            <Link
+              to={`/articles/${article.article_id}`}
+              onClick={() => scrollToTop()}
+            >
+              <h3 className="new-article__heading">{article.title}</h3>
+            </Link>
+            <p className="new-article__text">
+              {article.body.substring(0, 92) + "..."}
+            </p>
+          </motion.div>
+        </AnimatePresence>
       </div>
     );
   });
